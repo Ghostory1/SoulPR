008aacd7148c34eafc4c17d8fd605a34b3494a6e
unknown <ume6514@gmail.com>
2025-05-30 22:57:23 +0900

    Added Move and Look System

--- Source/SoulPR/Character/SPRCharacter.cpp
+++ Source/SoulPR/Character/SPRCharacter.cpp
@@ -2,13 +2,36 @@
 
 
 #include "Character/SPRCharacter.h"
-
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 // Sets default values
 ASPRCharacter::ASPRCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 카메라가 움직여도 캐릭터의 방향 유지
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->SetRelativeRotation(FRotator(-30.f,0.f,0.f));
+	// 입력 (마우스)를 돌리면 붐이 돌아가면서 카메라도 움직임
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+	//카메라가 직접 컨트롤러 회전을 사용하지 않도록 설정
+	// 대신 붐이 이미 회전을 따라가므로, 붐만 따라가게 하면 된다
+	FollowCamera->bUsePawnControlRotation = false;
 }
 
 // Called when the game starts or when spawned
@@ -24,11 +47,64 @@ void ASPRCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+void ASPRCharacter::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
 
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
 // Called to bind functionality to input
 void ASPRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//ETriggerEvent는 내가 계속 누르고있는지 체크
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASPRCharacter::Look);
+	}
+}
+
+void ASPRCharacter::Move(const FInputActionValue& Values)
+{
+	FVector2D MovementVector = Values.Get<FVector2D>();
+
+	// 컨트롤러에서 바라보는 방향 가져오기
+	if (Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		// Yaw값만 가져오면 된다. 바라보는 방향
+		const FRotator YawRotator(0, Rotation.Yaw, 0);
+		
+		// 
+		const FVector ForwardVector = FRotationMatrix(YawRotator).GetUnitAxis(EAxis::X);
+		const FVector RightVector = FRotationMatrix(YawRotator).GetUnitAxis(EAxis::Y);
+
+		//주어진 월드 방향 벡터(보통 정규화됨)를 따라 'ScaleValue'만큼 스케일된 이동 입력을 추가한다.
+		// ScaleValue가 0보다 작으면, 이동은 반대 방향으로 이루어진다.
+		// ScaleValue는 아날로그 입력에 사용될 수 있다
+		// 예를 들어, 0.5 같은 정상 값의 절반을 적용하고, -1.0은 방향을 반대로 한다.
+		AddMovementInput(ForwardVector, MovementVector.Y);
+		AddMovementInput(RightVector, MovementVector.X);
+
+	}
+}
+
+void ASPRCharacter::Look(const FInputActionValue& Values)
+{
+	FVector2D LookDirection = Values.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookDirection.X);
+		AddControllerPitchInput(LookDirection.Y);
+	}
 }
 
--- Source/SoulPR/Character/SPRCharacter.h
+++ Source/SoulPR/Character/SPRCharacter.h
@@ -4,26 +4,47 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
 #include "SPRCharacter.generated.h"
+
 
 UCLASS()
 class SOULPR_API ASPRCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+	
+	UPROPERTY(EditAnywhere)
+	class UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere)
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere)
+	class UInputAction* LookAction;
 public:
-	// Sets default values for this character's properties
 	ASPRCharacter();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+	// 컨트롤러가 빙의 됐을때 노티파이
+	virtual void NotifyControllerChanged() override;
+
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// 인풋 맵핑 함수
+	void Move(const FInputActionValue& Values);
+	void Look(const FInputActionValue& Values);
 };
